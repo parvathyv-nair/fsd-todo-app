7dc85fbda7636cff5dd51b5b2d63f725104627e9
parvathyv-nair <162776555+parvathyv-nair@users.noreply.github.com>
2025-02-04 20:09:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,5 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-function App() {
-    const [todos, setTodos] = useState([]);
-    const [newTodo, setNewTodo] = useState('');
-
-    useEffect(() => {
-        axios.get('https://your-backend-url.onrender.com/todos')
-            .then((res) => setTodos(res.data));
-    }, []);
-
-    const addTodo = () => {
-        axios.post('https://your-backend-url.onrender.com/todos', { text: newTodo })
-            .then((res) => {
-                setTodos([...todos, res.data]);
-                setNewTodo('');
-            });
-    };
-
-    return (
-        <div>
-            <h1>To-Do List</h1>
-            <input value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
-            <button onClick={addTodo}>Add</button>
-            <ul>
-                {todos.map((todo) => (
-                    <li key={todo._id}>{todo.text}</li>
-                ))}
-            </ul>
-        </div>
-    );
-}
-
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
